1f8f79b5d2432a3432e08e02a5ade8bc91d8bfa8
subhadip <onesubhadip@gmail.com>
2025-12-16 23:50:17 +0000

    Add nuxt.config and app.config

---
+++ app/app.config.ts
@@ -0,0 +1,17 @@
+export default defineAppConfig({
+  header: {
+    // Title to display if no logo
+    title: 'onesubhadip.dev',
+    // Logo configuration
+    logo: {
+      alt: '',
+      // Light mode
+      light: '',
+      // Dark mode
+      dark: ''
+    },
+  },
+  socials: {
+    linkedin: 'https://www.linkedin.com/in/deysubhadip/',
+  }
+})
